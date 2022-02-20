8e95c7df76dc23b6a3e11f8b3eeec56d6b36fff8
Justin R. St-Amant <jstamant24@gmail.com>
2022-02-19 22:09:29 -0600

    Laid-out buttons more intuitively

--- src/interface.js
+++ src/interface.js
@@ -3,11 +3,17 @@ import React from 'react';
 class Interface extends React.Component {
   render() {
     return(
-      <>
-        <button onClick={this.props.changeHaiku}>Next</button>
-        <button onClick={() => this.props.changeHaiku(false)}>Previous</button>
-        <button onClick={this.props.randomHaiku}>Random</button>
-      </>
+      <div id="interface">
+        <div className="half">
+          <button onClick={() => this.props.changeHaiku(false)}>Previous</button>
+        </div>
+        <div className="half">
+          <button onClick={this.props.changeHaiku}>Next</button>
+        </div>
+        <div className="full">
+          <button onClick={this.props.randomHaiku}>Random</button>
+        </div>
+      </div>
     );
   }
 }
