814a867d9c1d318c8aa592c159705f29c0b6a30c
Justin R. St-Amant <jstamant24@gmail.com>
2022-02-21 12:08:13 -0600

    Improved styling of the signature section of the haiku

--- src/haiku.js
+++ src/haiku.js
@@ -11,8 +11,8 @@ class Haiku extends React.Component {
           <p>{this.props.content[2]}</p>
         </div>
         <div className="signature">
-          <div className="date">{this.props.date}</div>
           <div className="counter">{this.props.selection+1}/{this.props.total}</div>
+          <div className="date">{this.props.date}</div>
           <div className="author">Justin St-Amant</div>
         </div>
       </div>
