e315f05b015d013bcd2034c2e9d0b344db560430
Justin R. St-Amant <jstamant24@gmail.com>
2022-02-22 20:04:47 -0600

    Added query string functionality and default haiku
    The page now shows the latest haiku, and displays a query string for the current
    haiku. The page will also accept a query string to select a specific haiku!

--- src/app.js
+++ src/app.js
@@ -8,27 +8,38 @@ import HaikuList from './haikulist';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // this.state.selection is the number of the selected haiku. Its array index
+    // is selection - 1
     this.state = {
       haikus: HaikuList,
-      selection: 0,
+      selection: 1,
       total: 0
     };
     this.state.total = this.state.haikus.length;
+    // Initially display the most recent haiku
+    this.state.selection = this.state.total;
+    // Or display the haiku as indicated by the query string, if valid
+    const url = new URL(window.location.href);
+    const haiku = url.searchParams.get('id');
+    if (1 <= haiku && haiku <= this.state.total)
+      this.state.selection = haiku;
+
     // Bind functions that are used in events and/or callbacks
     this.changeHaiku = this.changeHaiku.bind(this);
   }
 
   render() {
-    const title = this.state.haikus[this.state.selection].title;
-    const date = this.state.haikus[this.state.selection].date;
-    const content = this.state.haikus[this.state.selection].content;
+    this.updateQueryString(this.state.selection);
+    const title = this.state.haikus[this.state.selection-1].title;
+    const date = this.state.haikus[this.state.selection-1].date;
+    const content = this.state.haikus[this.state.selection-1].content;
     return(
       <>
         <Haiku
           title={title}
           date={date}
           content={content}
-          selection={this.state.selection}
+          selection={this.state.selection-1}
           total={this.state.total}
         />
         <Interface changeHaiku={this.changeHaiku}>TEST</Interface>
@@ -40,10 +51,10 @@ class App extends React.Component {
    * boundary conditions and prevents them.
    */
   setSelection(selection) {
-    if (selection >= this.state.total)
-      selection = this.state.total - 1;
-    if (selection < 0)
-      selection = 0;
+    if (selection > this.state.total)
+      selection = this.state.total;
+    if (selection <= 0)
+      selection = 1;
     this.setState({selection: selection});
   }
 
@@ -66,6 +77,16 @@ class App extends React.Component {
       this.setSelection(newSelection);
       break;
     }
+  }
+
+  /* Update the URL in the browser so that its query string matches the
+   * displayed haiku.
+   */
+  updateQueryString(id) {
+    // Using the window.history object allows us to prevent a reload upon
+    // changing the query string the way that the window.location object does.
+    let path = window.location.pathname + '?' + 'id=' + id;
+    window.history.pushState(null, '', path);
   }
 }
 
